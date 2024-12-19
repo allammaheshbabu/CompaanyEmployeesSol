533f72c27d18084571b58c1028078bb05e6e3daa
allammaheshbabu <allammaheshbabu232003@example.com>
2024-12-19 18:36:35 +0530

    Add project files.

---
+++ CompaanyEmployees/RepositoryContextDb/RepositoryContext.cs
@@ -0,0 +1,14 @@
+﻿using CompaanyEmployees.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CompaanyEmployees.RepositoryContextDb
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Company>? Companies { get; set; }
+        public DbSet<Employee>? Employees { get; set; }
+    }
+}
